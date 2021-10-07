e14adac5c152943956d70c7de253a1dbcec61e75
aayushali <aayushkhan51@gmail.com>
2021-10-07 10:07:15 +0545

    Handling POST request in API routes in NextJs

--- pages/comments/index.jsx
+++ pages/comments/index.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import { useState } from "react";
+
 // handling get request
+// and sending data to the api POST req too
 function CommentsPage() {
   const [comments, setComments] = useState([]);
+
+  const [comment, setComment] = useState(""); // This holds the input value
   const fetchComments = async () => {
     const response = await fetch("/api/comments");
     const data = await response.json();
     setComments(data);
   };
+
+  const submitComment = async () => {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <div>
+      <input
+        type="text"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      />
+      <button onClick={submitComment}>Submit Comment</button>
       <button onClick={fetchComments}>Load Comments</button>
       {comments.map((comment) => (
         <div key={comment.id}>
